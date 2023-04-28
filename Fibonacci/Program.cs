39c363a33f42fd0ca4a310e51728e9e7885fc729
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-04-28 23:41:29 +0200

    ForwardFibonacci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,7 @@
             : NaiveFibonacci(n - 1) + NaiveFibonacci(n - 2);
     static void Demonstrate(Func<int, long> fibonacci)
     {
-        int offset = 35;
+        int offset = 0;
         for (int i = 0; i < 10; i++)
             Console.WriteLine($"{offset + i}\t{fibonacci(offset + i)}");
         Console.WriteLine();
@@ -26,11 +26,26 @@
         return value;
     }
 
+    static IList<long> ForwardCache = new List<long>() { 0, 1 };
+
+    static long ForwardFibonacci(int n)
+    {
+        while (ForwardCache.Count <= n)
+        {
+            ForwardCache.Add(ForwardCache[ForwardCache.Count - 1] + ForwardCache[ForwardCache.Count - 2]);
+        }
+
+        return ForwardCache[n];
+    }
+
     private static void Main(string[] args)
     {
         Console.WriteLine("\nMemoizedFibonacci:\n");
         Demonstrate(MemoizedFibonacci);
         Console.WriteLine($"Cache count: {Cache.Count}");
+
+        Console.WriteLine("\nForwardFibonacci:\n");
+        Demonstrate(ForwardFibonacci);
 
         Console.WriteLine("\nNaiveFibonacci:\n");
         Demonstrate(NaiveFibonacci);
