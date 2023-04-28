e5266f48022d3b7e60c1a5c7786296365f6e1ec3
Cesar <8007837+cesaroll@users.noreply.github.com>
2023-04-28 22:34:54 +0200

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿static long NaiveFibonacci(int n) =>
+    n < 2
+        ? n
+        : NaiveFibonacci(n - 1) + NaiveFibonacci(n - 2);
+
+static void Demonstrate(Func<int, long> fibonacci)
+{
+    int offset = 33;
+    for (int i = 0; i < 10; i++)
+        Console.WriteLine($"{offset + i}\t{fibonacci(offset + i)}");
+    Console.WriteLine();
+}
+
+
+Demonstrate(NaiveFibonacci);
